7c66cf0b69ad8323f071eaa7182469f4b1632311
Yasin Akbulut <yasin@jotform.com>
2024-08-13 17:28:03 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# hotel
+ Hotel promotion web site
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Chip } from "@nextui-org/react";
+
+function App() {
+  return (
+    <Chip isDisabled color="primary">
+      Chip
+    </Chip>
+  );
+}
+
+export default App;
