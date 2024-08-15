f88814e042bee8d57bea57c93f720a713733d8b5
Yasin Akbulut <yasin@jotform.com>
2024-08-15 10:06:48 +0300

    coming soon section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import ComingSoon from "./components/ComingSoon";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Gallery from "./components/Gallery";
@@ -6,17 +7,25 @@ import Main from "./components/Main";
 import ServicesSection from "./components/ServicesSection";
 
 function App() {
+  const isComingSoon = true;
   return (
-    <main className="w-full">
-      <Main />
-      <section className="md:container mx-auto w-full py-8 px-4 flex flex-col gap-16">
-        <About />
-        <ServicesSection />
-        <Gallery />
-        <Contact />
-      </section>
-      <Footer />
-    </main>
+    <>
+      {isComingSoon ? (
+        <ComingSoon />
+      ) : (
+        <main className="w-full">
+          <ComingSoon />
+          <Main />
+          <section className="md:container mx-auto w-full py-8 px-4 flex flex-col gap-16">
+            <About />
+            <ServicesSection />
+            <Gallery />
+            <Contact />
+          </section>
+          <Footer />
+        </main>
+      )}
+    </>
   );
 }
 
---
+++ src/components/ComingSoon.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Logo from "../assets/img/logo.jpg";
+import { Image } from "@nextui-org/react";
+
+function ComingSoon() {
+  return (
+    <section class="w-full h-dvh bg-slate-200 flex flex-col gap-20 justify-center items-center">
+      <Image
+        src={Logo}
+        alt="Dört Yol Otel"
+        className="object-scale w-full h-auto max-w-[800px]"
+      />
+      <h2 class="text-center text-slate-800 lg:text-6xl md:text-5xl text-4xl font-bold">
+        Yakında Buradayız!
+      </h2>
+    </section>
+  );
+}
+
+export default ComingSoon;
