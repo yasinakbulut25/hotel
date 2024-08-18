1da80febb5f3804941c2e861b1840ec1be24cee3
Yasin Akbulut <yasin@jotform.com>
2024-08-18 14:52:23 +0300

    about text update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,12 +5,13 @@ function About() {
   return (
     <section id="hakkimizda" className="relative flex flex-col gap-8">
       <Title title="Hakkımızda" subTitle="HİKAYEMİZ" />
-      <p className="max-w-[920px] mx-auto text-pretty text-center">
+      <p className="max-w-[920px] mx-auto text-balance text-center">
         2017 yılından bu yana otelimiz, konuklarımıza huzur dolu ve konforlu bir
         konaklama sunma misyonuyla hizmet vermektedir. Misafirlerimize en yüksek
         kalitede hizmet sunarak, onlara evlerindeymiş gibi hissettirmek birinci
         önceliğimizdir. Siz değerli konuklarımızın her türlü ihtiyacını
-        karşılamak için titizlikle çalışmaktayız.
+        karşılamak için titizlikle çalışmaktayız. Tek ve çift kişilik
+        odalarımızda sizleri ağırlamaktan onur duyarız.
       </p>
     </section>
   );
